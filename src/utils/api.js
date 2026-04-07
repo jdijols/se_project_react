109774ef8f7cb890d60a8b2cc2b0c0fb7f4d39dc
Jason Dijols <jasondijols@gmail.com>
2026-04-07 16:01:52 -0500

    Fix port conflcits

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:3001";
 
 function handleResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
